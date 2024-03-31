2f6244b5afd08762dfb71ed7d9376f4c5a833400
Sayali Tugaonkar <sayalitugaonkar@gmail.com>
2024-03-31 16:13:44 +0530

    update-roles

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,22 @@
-
-provider "aws" {
-    region = "${var.AWS_REGION}"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 
+# Configure the AWS Provider
+provider "aws" {
+  region                   = "ap-south-1"
+  shared_credentials_files = ["/home/sayali/.credentials"]
+  profile                  = "testing"
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "Linux_Security_Gp"
-  vpc_id      = aws_vpc.main.id
-
   ingress {
     description      = "ssh from bastion"
     from_port        = 22
@@ -51,11 +60,11 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "ec2instance" {
   instance_type = "t3a.medium"
   ami = "${var.ami}" 
-  subnet_id = "subnet-fd7e1386"
+  subnet_id = "subnet-0d549bb0c0903c384"
   disable_api_termination = false
 
   ebs_optimized = false
-  key_name = "Sayali_mac"
+  key_name = "sayali"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   root_block_device {
     volume_size = "40"
